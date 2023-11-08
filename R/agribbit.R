7b5052a1afc41f9e62dbaf131d037666e76c38c2
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-11-08 22:56:57 +0900

    可愛い関数を追加

--- R/agribbit.R
+++ R/agribbit.R
@@ -233,4 +233,24 @@ agri.collect_shp <- function(pref_number) {
 }
 
 
+#' @title to look at cute frogs!!!
+#' @description \code{agri.frog_rain}
+#' @export
+
+agri.frog_rain <- function(n = 100){
+  for (i in 1:n){
+    r <- runif(1, 0, 1)
+    if (r >= 0.5) {
+      cat("🐸", "\n")
+    }
+    if (r < 0.5 & r >= 0.1){
+      cat("　", "\n")
+    }
+    if (r < 0.1){
+      cat("🥒", "\n")
+    }
+  }
+}
+
+
 
