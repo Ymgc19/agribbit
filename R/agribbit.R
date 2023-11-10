5fa6433a027f8cca9e6a951c0608bb5474fbf847
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-11-10 15:22:44 +0900

    agri.read_census()を実装

--- R/agribbit.R
+++ R/agribbit.R
@@ -259,3 +259,23 @@ agri.frog_rain <- function(n = 100){
 
 
 
+# 農林業センサスのデータを読み込んでそのままオブジェクト化
+#' @title read census data as object
+#' @description \code{agri.read_census}
+#' @export
+
+agri.read_census <- function(pref_code){
+  agribbit::agri.collect_census(pref_code)
+  if (pref_code <= 9){
+    pref_code <- as.character(paste("0", pref_code, sep = ""))
+  }
+  else{
+    pref_code <- pref_code
+  }
+  download_dir <- paste(as.character(pref_code), "農林業センサス2020", sep = "")
+  df <- agribbit::agri.read_as_csv(download_dir)
+  #  file.remove(file.path(download_dir)) # このファイルがemptyではないのでerrorが出てくる．
+  return(df)
+}
+
+
