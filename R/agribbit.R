f4fe7c527b14edd3ad90e39b72749f659c558a4c
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-11-10 15:08:33 +0900

    read_as_csv()をjoinで結合する形式に変換

--- R/agribbit.R
+++ R/agribbit.R
@@ -5,20 +5,19 @@
 agri.read_as_csv <- function(dir_folder){
   library(tidyverse)
   fs::dir_ls(here::here(dir_folder),
-           recurse = TRUE,
-           regexp = ".txt$") %>%
-  purrr::set_names(
-    fs::dir_ls(here::here(dir_folder)) %>%
-      basename() %>%
-      stringr::str_remove("（.+）")
-  ) %>%
-  purrr::map(
-    \(x) read_delim(x, delim = ",",
-                    locale = locale(encoding = "cp932"))
-  ) %>%
-  dplyr::bind_cols() %>%
-  mutate(KEY_CODE = KEY_CODE...1) %>%
-  dplyr::select(-contains("."))
+             recurse = TRUE,
+             regexp = ".txt$") %>%
+    purrr::set_names(
+      fs::dir_ls(here::here(dir_folder)) %>%
+        basename() %>%
+        stringr::str_remove("（.+）")
+    ) %>%
+    purrr::map(
+      \(x) read_delim(x, delim = ",",
+                      locale = locale(encoding = "cp932"))
+    ) %>%
+    reduce(left_join, by = "KEY_CODE") %>%
+    dplyr::select(-contains("."))
 }
 
 
--- 新しい関数向上.R
+++ 新しい関数向上.R
@@ -14,9 +14,44 @@ agri.read_census <- function(pref_code, place){
   }
   download_dir <- paste(as.character(pref_code), "農林業センサス2020", sep = "")
   df <- agribbit::agri.read_as_csv(download_dir)
-#  file.remove(file.path(download_dir))
+#  file.remove(file.path(download_dir)) # このファイルがemptyではないのでerrorが出てくる．
   return(df)
 }
 
 
-miyagi <- agri.read_census(4)
+miyagi <- agri.read_census(18)
+miyagi %>% glimpse()
+
+
+
+
+agri.read_as_csv <- function(dir_folder){
+  library(tidyverse)
+  fs::dir_ls(here::here(dir_folder),
+             recurse = TRUE,
+             regexp = ".txt$") %>%
+    purrr::set_names(
+      fs::dir_ls(here::here(dir_folder)) %>%
+        basename() %>%
+        stringr::str_remove("（.+）")
+    ) %>%
+    purrr::map(
+      \(x) read_delim(x, delim = ",",
+                      locale = locale(encoding = "cp932"))
+    ) %>%
+    reduce(left_join, by = "KEY_CODE") %>%
+    dplyr::select(-contains("."))
+}
+
+
+agri.read_as_csv("04農林業センサス2020")
+
+
+
+
+
+
+
+
+
+
