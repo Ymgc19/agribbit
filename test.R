bc551f7d5fb38787b5d98d7f4ac444a750626b60
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-11-26 17:15:21 +0900

    下限値を30に変更

--- test.R
+++ test.R
@@ -1,7 +1,25 @@
 # install.packages("devtools")
 devtools::install_github("Ymgc19/agribbit", force = T)
+library(agribbit)
+
 
 ishikawa <- agribbit::agri.read_census(17)
 ishikawa_fe <- ishikawa %>% agribbit::agri.fe_census()
 
 ishikawa_fe$fe_mean_age %>% table
+sum(is.na(ishikawa_fe$fe_mean_age))
+
+shp <- agri.read_census_shp(17)
+ishikawa_fe <- agri.join(
+  shp, ishikawa_fe
+)
+
+agri.fast_draw(ishikawa_fe, ishikawa_fe$fe_mean_work_days)
+
+a <- agri.interpolate(ishikawa_fe, "fe_mean_work_days")
+a$true.vs.predicted
+
+
+agri.join(shp, a$inputed) %>%
+  agri.fast_draw(., a$inputed$inputed_fe_mean_work_days)
+
